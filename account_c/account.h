721adc6ea06862e6d0ddadfe77a9e27de0764806
Nuclear-Catapult <nuclear.catapult@gmail.com>
2019-12-15 14:36:44 -0600

    change to notes

--- account_c/account.h
+++ account_c/account.h
@@ -9,7 +9,7 @@
 // They're limited from what C++ uses since C constructors only run once    //
 // before 'main()', and C destructors only run once after 'main()'. Due to  //
 // these restrictions, we will INSTEAD implement initializer and cleanup    //
-// functions, which have to be manually called every time we use them.      //
+// functions that have to be manually called every time we use them.        //
 //**************************************************************************//
 // C supports neither function overloading nor namespaces. If your program  //
 // gets big, you may have to append a struct name prefix to functions to    //
